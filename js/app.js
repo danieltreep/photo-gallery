00324b6c8a4194c96be1042c653bb69d4d3c2aa8
DanielTreep <danieltreep@live.nl>
2021-11-08 19:36:48 +0100

    tried to add custom search filter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
-const baguetteBox = require('baguettebox.js');
-baguetteBox.run('.gallery');
+
+function searchFilter() {
+    
+    // Gets the value typed in the searchbar and converts to lower case
+    let input = document.getElementById("search");
+    input = input.value.toLowerCase();
+
+    // Checking if there is an input value
+    console.log(input);
+
+    // Creates a variable for the photos
+    let photos = document.querySelectorAll("a");
+
+    // Target the data-caption attribute and converts it to lower case
+    let caption = photos[0].getAttribute("data-caption");
+    caption = caption.toLowerCase();
+    
+    // Checking if let caption targets data-caption attribute
+    console.log(caption);
+
+    for (let i = 0; i < caption.length; i++) {
+        
+        if (caption.includes(input)) {
+            photos[i].style.display = "block";
+        } else {
+            photos[i].style.display = "none";
+        }
+    }
+}
