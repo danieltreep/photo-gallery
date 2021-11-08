2c182d3b70127376603fefc4e02e5359bbdf23b5
DanielTreep <danieltreep@live.nl>
2021-11-08 22:39:28 +0100

    added custom search filter

--- js/app.js
+++ js/app.js
@@ -11,20 +11,15 @@ function searchFilter() {
     // Creates a variable for the image
     let image = document.querySelectorAll("a");
 
-    // Target the data-caption attribute and converts it to lower case. 
-    // The caption variable only targets the caption I select between [].
-    let caption = image[0].getAttribute("data-caption");
-    caption = caption.toLowerCase();
-    
-    // Checking if let caption targets data-caption attribute
-    console.log(caption);
+        for (let i = 0; i < image.length; i++) {
 
-    for (let i = 0; i < caption.length; i++) {
-        
-        if (caption.includes(input)) {
-            image[i].style.display = "block";
-        } else {
-            image[i].style.display = "none";
-        }
+            let caption = image[i].getAttribute("data-caption");
+            caption = caption.toLowerCase();
+    
+            if (caption.includes(input)) {
+                image[i].style.display = "block";
+            } else {
+                image[i].style.display = "none";
+            }
     }
 }
